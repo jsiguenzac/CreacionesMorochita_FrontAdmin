d070d33d97fcb6515fb6246796aef8afcd734f64
jsiguenzac <joelsantiago.pes@gmail.com>
2024-11-21 00:12:35 -0500

    Integracion de api dev

--- src/config/deployMode.js
+++ src/config/deployMode.js
@@ -1,8 +1,8 @@
 export const IS_PRODUCTION = false;
 const API_URL_GROUP = {
     LOCAL_DEV: "http://127.0.0.1:8000",
-    DEV: "http://",
+    DEV: "https://api-morochita-dev.onrender.com",
     TEST: "http://",
     PROD: ""  
 };
-export const API_URL = IS_PRODUCTION ? API_URL_GROUP.DEV : API_URL_GROUP.LOCAL_DEV; 
+export const API_URL = !IS_PRODUCTION ? API_URL_GROUP.DEV : API_URL_GROUP.LOCAL_DEV; 
